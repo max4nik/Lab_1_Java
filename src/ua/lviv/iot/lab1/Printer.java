33118b7541255634dc1e3bfe1745251b4c770fef
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-01-23 12:03:52 +0200

    Adding class Printer for lab1

---
+++ src/ua/lviv/iot/lab1/Printer.java
@@ -0,0 +1,126 @@
+package ua.lviv.iot.lab1;
+
+public class Printer {
+    private String name;
+    private double performancePagePerMinute;
+    private int priceInUSDDollars;
+    private double wageInKg;
+    private String mark;
+    private static double powerConsumptionInWatt = 1.2;
+    protected Boolean isColor;
+    protected String wayToConnect;
+
+    public Printer() {
+    }
+
+    public Printer(String name, double performancePagePerMinute, int priceInUSDDollars, double wageInKg, String mark, Boolean isColor, String wayToConnect) {
+        this.name = name;
+        this.performancePagePerMinute = performancePagePerMinute;
+        this.priceInUSDDollars = priceInUSDDollars;
+        this.wageInKg = wageInKg;
+        this.mark = mark;
+        this.isColor = isColor;
+        this.wayToConnect = wayToConnect;
+    }
+
+    public Printer(String name, double performancePagePerMinute, int priceInUSDDollars, double wageInKg) {
+        this(name, performancePagePerMinute, priceInUSDDollars, wageInKg, null, null, null);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPerformancePagePerMinute() {
+        return performancePagePerMinute;
+    }
+
+    public void setPerformancePagePerMinute(int performancePagePerMinute) {
+        this.performancePagePerMinute = performancePagePerMinute;
+    }
+
+    public int getPriceInUSDDollars() {
+        return priceInUSDDollars;
+    }
+
+    public void setPriceInUSDDollars(int priceInUSDDollars) {
+        this.priceInUSDDollars = priceInUSDDollars;
+    }
+
+    public double getWageInKg() {
+        return wageInKg;
+    }
+
+    public void setWageInKg(int wageInKg) {
+        this.wageInKg = wageInKg;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+
+    public static void setPowerConsumptionInWatt(double powerConsumptionInWatt) {
+        Printer.powerConsumptionInWatt = powerConsumptionInWatt;
+    }
+
+    public static double getPowerConsumptionInWatt() {
+        return powerConsumptionInWatt;
+    }
+
+    public Boolean getIsColor() {
+        return isColor;
+    }
+
+    public void setIsColor(Boolean color) {
+        isColor = color;
+    }
+
+    public String getWayToConnect() {
+        return wayToConnect;
+    }
+
+    public void setWayToConnect(String wayToConnect) {
+        this.wayToConnect = wayToConnect;
+    }
+
+    @Override
+    public String toString() {
+        return "Printer{" +
+                "name='" + name + '\'' +
+                ", performance_page_per_sec=" + performancePagePerMinute +
+                ", priceInUSDDollars=" + priceInUSDDollars +
+                ", wageInKG=" + wageInKg +
+                ", mark='" + mark + '\'' +
+                ", isColor=" + isColor +
+                ", wayToConnect='" + wayToConnect + '\'' +
+                '}';
+    }
+
+    public static void printStaticField() {
+        System.out.println("From static method: " + Printer.powerConsumptionInWatt);
+
+    }
+
+    public void printField() {
+        System.out.println("From non-static method: " + Printer.powerConsumptionInWatt);
+    }
+
+    public void resetValues(String name, double performance_page_per_sec, int priceInUSDDollars, double wageInKG, String mark, Boolean isColor, String wayToConnect) {
+        this.name = name;
+        this.performancePagePerMinute = performance_page_per_sec;
+        this.priceInUSDDollars = priceInUSDDollars;
+        this.wageInKg = wageInKG;
+        this.mark = mark;
+        this.isColor = isColor;
+        this.wayToConnect = wayToConnect;
+    }
+}
